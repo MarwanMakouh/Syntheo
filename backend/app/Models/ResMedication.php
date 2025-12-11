cbe9b88cc1d16ed25dd4c9e3b10ca742966f790c
MarwanMakouh <marwan.makouh@student.ehb.be>
2025-12-11 15:44:19 +0100

    Create ResMedication.php

---
+++ backend/app/Models/ResMedication.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ResMedication extends Model
+{
+    use HasFactory;
+
+    protected $table = 'res_medication';
+    protected $primaryKey = 'res_medication_id';
+
+    protected $fillable = [
+        'medication_id',
+        'resident_id',
+        'is_active',
+        'end_date',
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+        'end_date' => 'date',
+    ];
+
+    public function medication()
+    {
+        return $this->belongsTo(\App\Models\MedicationLibrary::class, 'medication_id', 'medication_id');
+    }
+
+    public function resident()
+    {
+        return $this->belongsTo(Resident::class, 'resident_id', 'resident_id');
+    }
+
+    public function schedules()
+    {
+        return $this->hasMany(\App\Models\ResSchedule::class, 'res_medication_id', 'res_medication_id');
+    }
+}
