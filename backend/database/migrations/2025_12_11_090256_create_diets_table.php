27eb38a568f19c1833e2562b4b3e316969ef1e19
MarwanMakouh <marwan.makouh@student.ehb.be>
2025-12-11 10:03:31 +0100

    diet migration aanmaken

---
+++ backend/database/migrations/2025_12_11_090256_create_diets_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('diets', function (Blueprint $table) {
+            $table->id('diet_id');
+            $table->string('diet_type');
+            $table->foreignId('resident_id')->constrained('residents', 'resident_id')->onDelete('cascade');
+            $table->text('description')->nullable();
+            $table->timestamp('created_at');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('diets');
+    }
+};
