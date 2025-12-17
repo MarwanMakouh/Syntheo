c313d7a97f35990615df0e36f0a55b90da422643
Arda-exe <aydemirarda10@gmail.com>
2025-12-17 10:22:32 +0100

    styling constants for layout

--- frontend/constants/index.ts
+++ frontend/constants/index.ts
@@ -5,3 +5,15 @@
  */
 
 export { Colors, type ColorKey } from './colors';
+export { Spacing, Layout, type SpacingKey, type LayoutKey } from './spacing';
+export {
+  Typography,
+  FontSize,
+  FontWeight,
+  LineHeight,
+  type FontSizeKey,
+  type FontWeightKey,
+  type TypographyKey,
+} from './typography';
+export { Shadows, type ShadowKey } from './shadows';
+export { BorderRadius, getCircleRadius, type BorderRadiusKey } from './borderRadius';
