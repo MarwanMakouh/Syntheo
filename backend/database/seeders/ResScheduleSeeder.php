ba8d356c6b726b90bf9b3b9dca3aa9d7cd160908
MarwanMakouh <marwan.makouh@student.ehb.be>
2025-12-19 18:41:07 +0100

    seeders en migrations toevoegen

--- backend/database/seeders/ResScheduleSeeder.php
+++ backend/database/seeders/ResScheduleSeeder.php
@@ -16,16 +16,16 @@ class ResScheduleSeeder extends Seeder
                 'res_medication_id' => 1,
                 'dosage' => '500mg',
                 'instructions' => 'Met ontbijt innemen',
-                'time_of_day' => '08:00:00',
-                'day_of_week' => 'Dagelijks',
+                'time_of_day' => 'Ochtend', // Changed from '08:00:00' to 'Ochtend'
+                'day_of_week' => null, // Changed from 'Dagelijks' to null (null = daily)
             ],
             [
                 'schedule_id' => 2,
                 'res_medication_id' => 1,
                 'dosage' => '500mg',
                 'instructions' => 'Met avondeten innemen',
-                'time_of_day' => '18:00:00',
-                'day_of_week' => 'Dagelijks',
+                'time_of_day' => 'Avond', // Changed from '18:00:00' to 'Avond'
+                'day_of_week' => null, // Changed from 'Dagelijks' to null (null = daily)
             ],
             // Ramipril voor Maria Peeters
             [
@@ -33,8 +33,8 @@ class ResScheduleSeeder extends Seeder
                 'res_medication_id' => 2,
                 'dosage' => '5mg',
                 'instructions' => 'Op nuchtere maag',
-                'time_of_day' => '07:00:00',
-                'day_of_week' => 'Dagelijks',
+                'time_of_day' => 'Ochtend', // Changed from '07:00:00' to 'Ochtend'
+                'day_of_week' => null, // Changed from 'Dagelijks' to null (null = daily)
             ],
             // Simvastatine voor Jozef Willems
             [
@@ -42,8 +42,8 @@ class ResScheduleSeeder extends Seeder
                 'res_medication_id' => 3,
                 'dosage' => '40mg',
                 'instructions' => 'Voor het slapen gaan',
-                'time_of_day' => '21:00:00',
-                'day_of_week' => 'Dagelijks',
+                'time_of_day' => 'Nacht', // Changed from '21:00:00' to 'Nacht'
+                'day_of_week' => null, // Changed from 'Dagelijks' to null (null = daily)
             ],
             // Paracetamol voor Anna Claes
             [
@@ -51,8 +51,8 @@ class ResScheduleSeeder extends Seeder
                 'res_medication_id' => 4,
                 'dosage' => '1000mg',
                 'instructions' => 'Bij pijn, max 4x per dag',
-                'time_of_day' => '09:00:00',
-                'day_of_week' => 'Dagelijks',
+                'time_of_day' => 'Ochtend', // Changed from '09:00:00' to 'Ochtend'
+                'day_of_week' => null, // Changed from 'Dagelijks' to null (null = daily)
             ],
             // Omeprazol voor Jozef Willems
             [
@@ -60,8 +60,8 @@ class ResScheduleSeeder extends Seeder
                 'res_medication_id' => 5,
                 'dosage' => '20mg',
                 'instructions' => 'Voor het ontbijt',
-                'time_of_day' => '07:30:00',
-                'day_of_week' => 'Dagelijks',
+                'time_of_day' => 'Ochtend', // Changed from '07:30:00' to 'Ochtend'
+                'day_of_week' => null, // Changed from 'Dagelijks' to null (null = daily)
             ],
             // Rivaroxaban voor Frans Mertens
             [
@@ -69,8 +69,8 @@ class ResScheduleSeeder extends Seeder
                 'res_medication_id' => 6,
                 'dosage' => '20mg',
                 'instructions' => 'Met avondeten',
-                'time_of_day' => '18:00:00',
-                'day_of_week' => 'Dagelijks',
+                'time_of_day' => 'Avond', // Changed from '18:00:00' to 'Avond'
+                'day_of_week' => null, // Changed from 'Dagelijks' to null (null = daily)
             ],
         ]);
     }
