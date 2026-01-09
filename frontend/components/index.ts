2cac443d24d0a8a4a9d0f7e76539af2f10d345ab
BrendDG <brend.de.greef@student.ehb.be>
2026-01-09 11:43:15 +0100

    roleguard
    controller zodat als je bent ingelogd als admin of een andere rol dan kan je niet van rol veranderen. Het is een extra controle

--- frontend/components/index.ts
+++ frontend/components/index.ts
@@ -4,6 +4,9 @@ export * from './navigation/navigation-bar';
 // UI Components
 export * from './ui/search-bar';
 
+// Auth Components
+export * from './RoleGuard';
+
 // Announcement Components
 export * from './announcement-create-modal';
 export * from './announcements-modal';
